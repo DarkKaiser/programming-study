eca3be01cbc9e28e3c3584a6394e292b33a981b0
DarkKaiser <darkkaiser78@hotmail.com>
2021-09-02 14:26:09 +0900

    no message

---
+++ Web/React/event_handling/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import EventPractice from './EventPractice';
+import EventPracticeFunction from './EventPractice_Function';
+
+function App() {
+  return (
+    <div>
+      <EventPractice />
+      <br />
+      <EventPracticeFunction />
+    </div>
+  )
+}
+
+export default App;
